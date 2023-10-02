2756ce1485717e1a074949344b094518fd4a0b26
abdallah279 <abdallahzeko8@gmail.com>
2023-10-03 00:56:18 +0300

    add services page

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,3 +93,71 @@ $(document).ready(function () {
     });
   });
 });
+
+/************ tabs ***********/
+$(".tabs-show").css("display", "none");
+getProFromLocal();
+
+$(".tabs-m").on("click", function (e) {
+  e.preventDefault();
+
+  // FadeOut All Content
+  $(".tabs-show").fadeOut(0);
+  let item = $(this).attr("data-show");
+
+  // Show Current Content
+  $(`.${item}`).fadeIn();
+
+  // Active Class In Tabs
+  $(".tabs-m").removeClass("active").parent().removeClass("active");
+  $(this).addClass("active").parent().addClass("active");
+
+  // Set Item
+  addLocal(item);
+
+  $(".overlay-m").click();
+});
+
+// Add activeTab To Local storage
+function addLocal(tab) {
+  sessionStorage.setItem("activeTab", JSON.stringify(tab));
+}
+
+// Remove activeTab To Local storage
+function removeLocal() {
+  sessionStorage.removeItem("activeTab");
+}
+
+// Get activeTab From Local Storage
+function getProFromLocal() {
+  let data = sessionStorage.getItem("activeTab");
+
+  // Check if Theres activeTab In Local Storage
+  if (data) {
+    activeTab = JSON.parse(data);
+    $(`.${activeTab}`).fadeIn();
+    $(".tabs-m").removeClass("active").parent().removeClass("active");
+    $(`[data-show=${activeTab}]`)
+      .addClass("active")
+      .parent()
+      .addClass("active");
+  } else {
+    $(".show-form").fadeIn(0).addClass('active');
+  }
+}
+
+// Fade Out Section
+$(".close-services").on("click", function (e) {
+  e.preventDefault();
+  $(this).closest('.services-det-item').fadeOut();
+  $(".tabs-m").removeClass("active").parent().removeClass("active");
+  removeLocal();
+});
+
+
+$('[data-pass]').on('click', function(){
+  let item = $(this).attr('data-pass')
+
+  // Set Item
+  addLocal(item);
+})
